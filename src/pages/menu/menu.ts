bcd3f3579e0ef4ea69a935b23cb488d3b9621e19
DJ Cyphers <djcyphers@gmail.com>
2018-04-23 05:22:08 -0700

    Alerts, Toasts and Loading

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { Dish } from '../../shared/dish';
 import { DishProvider } from '../../providers/dish/dish';
 import { FavoriteProvider } from '../../providers/favorite/favorite';
@@ -19,11 +19,15 @@ import { DishdetailPage } from '../dishdetail/dishdetail';
 export class MenuPage implements OnInit {
   dishes: Dish[];
   errMess: string;
+  favorite: boolean;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     private dishservice: DishProvider,
     private favoriteservice: FavoriteProvider,
-    @Inject('BaseURL') private BaseURL) { }
+    private toastCtrl: ToastController,
+    @Inject('BaseURL') private BaseURL ) { 
+
+    }
   
   ngOnInit() {
     this.dishservice.getDishes()
@@ -44,6 +48,10 @@ export class MenuPage implements OnInit {
 
   addToFavorites(dish: Dish) {
     console.log('Adding to Favorites', dish.id);
-    this.favoriteservice.addFavorite(dish.id);
+    this.favorite = this.favoriteservice.addFavorite(dish.id);
+    this.toastCtrl.create({
+      message: 'Dish ' + dish.id + ' added as favorite successfully',
+      position: 'middle',
+      duration: 3000}).present();
   }
 }
