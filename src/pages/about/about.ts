15e86443f3753e0b79f7bc1e1d7f3566d865a839
DJ Cyphers <djcyphers@gmail.com>
2018-04-23 03:03:17 -0700

    IonicAssignmentWeek1

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, Inject } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Leader } from '../../shared/leader';
+import { LeaderProvider } from '../../providers/leader/leader';
+
 
 /**
  * Generated class for the AboutPage page.
@@ -15,11 +18,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  errMess: string;
+  leaders: Leader[];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private leaderProvider: LeaderProvider,
+  @Inject('BaseURL') private BaseURL) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AboutPage');
+  }
+
+  ngOnInit() {
+
+    this.leaderProvider.getLeaders().subscribe(leaders => this.leaders = leaders, errmess => this.errMess = <any>errmess);
+
   }
 
 }
